dca470ef70a1f831c3b71057da0b22766be6eee2
khushi <khushi7mittal@gmail.com>
2022-04-30 01:46:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ButtonAppBar from './component/Navbar';
+import ToDoList from './component/ToDoList';
+
+function App() {
+  return(
+    <div>
+      <ButtonAppBar/> 
+      <ToDoList /> 
+    </div>
+  )
+}
+
+export default App;
